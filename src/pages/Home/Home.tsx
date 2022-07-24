613a295a9648ece8b0976eafa6757e37be385f4c
Abhishek Jain <abhishek.jain4@shell.com>
2022-07-24 21:48:18 +0100

    add chakra react select

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,34 +1,58 @@
-import { usePortfoliosQuery, useTransactionsAggregateQuery } from '../../generated/graphql';
-import { Box, Select } from '@chakra-ui/react';
-import { useState } from 'react';
+import {
+  TransactionsAggregateQuery,
+  usePortfoliosQuery,
+  useTransactionsAggregateQuery,
+} from '../../generated/graphql';
+import { Box, Text } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { Select as ChakraSelect } from 'chakra-react-select';
+
+type Option = { value: string; label: string };
 
 const Home = () => {
-  const [selectedPortfolio, setSelectedPortfolio] = useState<string | null>(null);
-  const { data: portfolios, loading: portfoliosLoading } = usePortfoliosQuery();
+  const [selectedPortfolio, setSelectedPortfolio] = useState<Array<Option> | undefined>(undefined);
   const { data, loading } = useTransactionsAggregateQuery({
     variables: {
-      pfIds: selectedPortfolio,
+      pfIds: selectedPortfolio?.map((x) => x.value)?.join(','),
     },
-    skip: !selectedPortfolio,
+    skip: !selectedPortfolio?.length,
   });
 
-  console.log('data, loading', data, loading);
-  console.log('portfolios, loading', portfolios?.portfolios, portfoliosLoading);
+  const [latestData, setLatestData] = useState<TransactionsAggregateQuery | undefined>(undefined);
+
+  useEffect(() => {
+    console.log('selectedPortfolio, data', selectedPortfolio, data);
+    if (data) {
+      setLatestData(data);
+    }
+    if (!selectedPortfolio?.length) {
+      setLatestData(undefined);
+    }
+  }, [data, selectedPortfolio]);
+
+  const { data: portfolios } = usePortfoliosQuery();
+
+  const options = portfolios?.portfolios?.map((x) => ({
+    value: x?._id ?? '',
+    label: x?.name ?? '',
+  }));
 
   return (
     <div>
       <Box p={4}>
-        <Select
-          w={300}
-          mb={4}
-          value={selectedPortfolio as string}
-          onChange={(e) => {
-            setSelectedPortfolio(e.target.value);
-          }}>
-          {portfolios?.portfolios?.map((x) => {
-            return <option value={x?._id as string}>{x?.name}</option>;
-          })}
-        </Select>
+        <ChakraSelect<Option, true>
+          isMulti
+          options={options}
+          value={selectedPortfolio}
+          onChange={(value) => {
+            setSelectedPortfolio([...value]);
+          }}
+        />
+        <Box opacity={loading ? 0.5 : 1}>
+          <Text>{latestData?.transactionsAggregate?.cash?.cashIn ?? 0}</Text>
+          <Text>{latestData?.transactionsAggregate?.cash?.cashOut ?? 0}</Text>
+          <Text>{latestData?.transactionsAggregate?.cash?.currentCash ?? 0}</Text>
+        </Box>
       </Box>
     </div>
   );
